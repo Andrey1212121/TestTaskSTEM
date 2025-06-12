f85701fca1344a6c9cc1813717922575403cda34
Andrey1212121 <andreyhara2005@gmail.com>
2025-06-13 01:49:45 +0300

    CoreMechanics

--- Assets/FigureAnimal.cs
+++ Assets/FigureAnimal.cs
@@ -6,25 +6,42 @@ public class FigureAnimal : MonoBehaviour
 {
     public SpriteRenderer shapeFigure;
     public SpriteRenderer animalFigure;
-    
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string ID;
+    public GameManager gameManager;
+    private bool isTouched = false;
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0); 
 
+            if (touch.phase == TouchPhase.Began) 
+            {
+                // Преобразуем координаты касания в мировые координаты
+                Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+                touchPosition.z = 0; 
+                
+                Collider2D targetObject = Physics2D.OverlapPoint(touchPosition);
+
+                if (targetObject == GetComponent<Collider2D>() && !isTouched) 
+                {
+                    isTouched = true;
+                    gameManager.AddFigure(this);
+                }
+            }
+        }
     }
-
-    public void SetInfo(Sprite shape, Sprite animal, Color color)
+    public void SetInfo(Sprite shape, Sprite animal, Color color, string id)
     {
         shapeFigure.sprite = shape;
         animalFigure.sprite = animal;
         shapeFigure.color = color;
         PolygonCollider2D polygonCollider = gameObject.AddComponent<PolygonCollider2D>();
-        
+        ID = id;
+
     }
 }
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -10,6 +10,9 @@ public class GameManager : MonoBehaviour
     public Color[] colorFigure;
     public GameObject prefabFigure;
     public int countTrio = 7;
+
+    public List<FigureAnimal> figureAnimalsSelected = new List<FigureAnimal>(7);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,22 +21,78 @@ public class GameManager : MonoBehaviour
             int randomIndexShape = UnityEngine.Random.Range(0, spritesShape.Length);
             int randomIndexAnimal = UnityEngine.Random.Range(0, spritesAnimal.Length);
             int randomIndexColor = UnityEngine.Random.Range(0, colorFigure.Length);
+            string id = randomIndexShape.ToString() + randomIndexAnimal.ToString() + randomIndexColor.ToString();
             for (int j = 0; j < 3; j++)
             {
                 GameObject figure = Instantiate(prefabFigure, transform.position, transform.rotation);
-                Debug.Log(randomIndexAnimal);
-                Debug.Log(randomIndexShape);
-                Debug.Log(randomIndexColor);
-                figure.GetComponent<FigureAnimal>().SetInfo(spritesShape[randomIndexShape], spritesAnimal[randomIndexAnimal], colorFigure[randomIndexColor]);
-
+                figure.GetComponent<FigureAnimal>().gameManager = this;
+                //Debug.Log(randomIndexAnimal);
+                //Debug.Log(randomIndexShape);
+                //Debug.Log(randomIndexColor);
+                //Debug.Log(id);
+                figure.GetComponent<FigureAnimal>().SetInfo(spritesShape[randomIndexShape], spritesAnimal[randomIndexAnimal], colorFigure[randomIndexColor], id);
             }
-            
+
         }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void AddFigure(FigureAnimal selectFigure)
     {
-        
+        figureAnimalsSelected.Add(selectFigure);
+        CheckFigures();
+        Debug.Log(figureAnimalsSelected);
     }
+
+    private void CheckFigures()
+    {
+        Dictionary<string, int> idCounts = new Dictionary<string, int>();
+
+        foreach (FigureAnimal figure in figureAnimalsSelected)
+        {
+            if (idCounts.ContainsKey(figure.ID))
+            {
+                idCounts[figure.ID]++;
+            }
+            else
+            {
+                idCounts[figure.ID] = 1;
+            }
+        }
+
+        foreach (var pair in idCounts)
+        {
+            if (pair.Value >= 3)
+            {
+                DeleteThree(pair.Key); 
+                break;
+            }
+        }
+    }
+
+    private void DeleteThree(string DeletedID) 
+    {
+        List<FigureAnimal> toRemove = new List<FigureAnimal>();
+        //int count = 0;
+
+        
+        foreach (FigureAnimal figure in figureAnimalsSelected)
+        {
+            if (figure.ID == DeletedID)
+            {
+                toRemove.Add(figure);
+              //  count++;
+            }
+        }
+
+        // Удаляем найденные фигуры
+        foreach (FigureAnimal figure in toRemove)
+        {
+            figureAnimalsSelected.Remove(figure); 
+            Destroy(figure.gameObject);            
+        }
+
+        toRemove.Clear();
+    }
+
 }
